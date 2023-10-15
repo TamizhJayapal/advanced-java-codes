d96ca77fdf2b4d086626b5f40b2df9e25b3accf4
Tamizhazhagan Jayapal <40054684+TamizhJayapal@users.noreply.github.com>
2023-10-15 22:58:32 +0530

    Update DemoServlet.java

--- DemoServlet.java
+++ DemoServlet.java
@@ -1,15 +1,37 @@
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+package com.demo;
 
-public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html");
-        response.getWriter().println("<html><head><title>Hello, World Servlet</title></head><body>");
-        response.getWriter().println("<h1>Hello, World!</h1>");
-        response.getWriter().println("</body></html>");
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class DemoServlet extends GenericServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public DemoServlet() {
+        super();
     }
+    
+    public void init() {
+    	System.out.println("servlet started");
+    }
+    
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		String name = request.getParameter("username");
+		
+		out.println("<html><head><title>GenericServlet</title></head><body>");
+        out.println("<h1>Hello, " +  name + "!</h1>");
+        out.println("</body></html>");
+	}
+	
+	public void destroy() {
+		System.out.println("servlet ended");
+	}
+
 }
